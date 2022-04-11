197cf422602ae2c3dbbe76dd8a317e913dee1965
Heylga <oy.golovina@gmail.com>
2022-04-11 11:50:57 +0000

    simple counter created

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,40 @@
-import React from "react";
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import PropTypes from "prop-types";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// //include images into your bundle
+// import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+// //create your first component
+// const Home = () => {
+// 	const [counter, countUp] = useState(0);
 
-export default Home;
+// 	useEffect(() => {
+// 		const interval = setInterval(() => {
+// 			console.log();
+// 		}, 1000);
+// 		return () => clearInterval(interval);
+// 	}, []);
+// 	return (
+// 		<div>
+// 			<h1 className="text-center mt-5">Simple Counter</h1>
+// 			<h2>{counter} seconds</h2>
+// 			<p
+// 				className="seconds text-center mt-5"
+// 				style={{ fontSize: "5rem" }}
+// 				onClick={() => countUp(counter + 1)}>
+// 				⌛
+// 			</p>
+// 			<a href="#" className="btn btn-success">
+// 				If you see this green button... bootstrap is working...
+// 			</a>
+// 			<p>
+// 				Made by{" "}
+// 				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+// 				love!
+// 			</p>
+// 		</div>
+// 	);
+// };
+
+// export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
@@ -8,5 +9,44 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+function SimpleCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="clock">🕑</div>
+			<div className="four">{props.digitFour}</div>
+			<div className="three">{props.digitThree}</div>
+			<div className="two">{props.digitTwo}</div>
+			<div className="one">{props.digitOne}</div>
+		</div>
+	);
+}
+
+SimpleCounter.PropTypes = {
+	digitFour: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitOne: PropTypes.number,
+};
+
+let counter = 0;
+setInterval(function () {
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+	console.log(four, three, two, one);
+
+	counter++;
+
+	ReactDOM.render(
+		<SimpleCounter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
